cc9f271d2dca8a9aaa13839a0d59c22f14d53347
Mike Kipnis <mike.kipnis@gmail.com>
2025-06-01 02:33:34 +0000

    ubuntu build test

--- build_with_cmake.sh
+++ build_with_cmake.sh
@@ -4,9 +4,12 @@ rm -rf build
 cmake -B build -S . -DCMAKE_INSTALL_PREFIX=$HOME
 cmake --build build --verbose --target install
 
+# To install on ubuntu
+#python3 -m pip install --upgrade build
+# sudo apt-get install swig
 CMAKE_ARGS="-DLIQUIBOOK_SRC=`pwd`/src" python3 -m build --wheel python --verbose
 
-unzip -l python/dist/liquibook-1.0.1-cp313-cp313-macosx_15_0_arm64.whl
+unzip -l python/dist/*
 
-echo To install: pip3 install python/dist/liquibook-1.0.1-cp313-cp313-macosx_15_0_arm64.whl --break-system-packages --force-reinstall
+echo To install: pip3 install python/dist/*  --break-system-packages --force-reinstall
 echo To test: python3 python/basic_order_book_test.py
