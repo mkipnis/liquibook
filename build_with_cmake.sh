d86a5cb08086ee68ddf609528d6fe3f724e5ba2b
Mike Kipnis <mike.kipnis@gmail.com>
2025-06-01 02:41:40 +0000

    updated for ubuntu

--- build_with_cmake.sh
+++ build_with_cmake.sh
@@ -6,7 +6,7 @@ cmake --build build --verbose --target install
 
 CMAKE_ARGS="-DLIQUIBOOK_SRC=`pwd`/src" python3 -m build --wheel python --verbose
 
-unzip -l python/dist/liquibook-1.0.1-cp313-cp313-macosx_15_0_arm64.whl
+unzip -l python/dist/*
 
-echo To install: pip3 install python/dist/liquibook-1.0.1-cp313-cp313-macosx_15_0_arm64.whl --break-system-packages --force-reinstall
+echo To install: pip3 install python/dist/* --break-system-packages --force-reinstall
 echo To test: python3 python/basic_order_book_test.py
