57e9cb2a089f0127371236dba7ff5843304455bf
Mike Kipnis <mike.kipnis@gmail.com>
2025-08-28 21:43:43 -0400

    updated build

--- build_with_cmake.sh
+++ build_with_cmake.sh
@@ -7,7 +7,5 @@ cmake --build build --target install
 # Build wheel via scikit-build / pyproject.toml
 CMAKE_ARGS="-DLIQUIBOOK_SRC=$(pwd)/src -DBUILD_PYTHON=ON" python3 -m build --wheel --verbose
 
-unzip -l python/dist/*
-
 echo To install: pip3 install dist/* --break-system-packages --force-reinstall
 echo To test: python3 python/examples/BasicOrderBookTest/basic_order_book_test.py
